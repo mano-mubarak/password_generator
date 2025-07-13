ba3ad2575e2ba27d0a901d4d0d496c95f647e734
Abdulrahman Mubarak <abdomubarak25@gmail.com>
2025-07-13 10:40:19 -0700

    Add files via upload

---
+++ password.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+import string
+import random
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def a():
+    password = ''
+    letters = list(string.ascii_letters)+list("!@#$%^&*()0123456789")
+    Number_of_characters = 15
+    if request.method == 'POST':
+        Number_of_characters = int(request.form.get('num'))
+        password = ''.join(random.sample(letters, Number_of_characters))
+        with open("password.txt","a") as password_file:
+            password_file.write(password + '\n')
+    return render_template('password.html', password=password, num=Number_of_characters)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
